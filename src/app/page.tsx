3a5eeace1f4894466089b0dbca22fb207dd9ccb4
Snow Yáng <snow.yang@fluv.com>
2024-07-21 19:23:37 +0800

    p add framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import NativeModal from "./components/NativeModal";
-
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
-    <main className="min-h-[100svh] max-w-md w-full border border-slate-400 rounded-3xl mx-auto">
-      <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
-      <NativeModal
-        isModalOpen={isModalOpen}
-        closeModal={() => setIsModalOpen(false)}
-      >
-        <p>Modal Content</p>
-      </NativeModal>
+    <main className="min-h-[100svh] max-w-md w-full border border-slate-400 rounded-3xl mx-auto px-3 py-3">
+      <h1>Rich</h1>
     </main>
   );
 }
