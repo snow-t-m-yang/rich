7e6f4e88b57246e0156e54eb372f47a30c776d3b
Snow Yáng <snow.yang@fluv.com>
2024-07-21 21:32:32 +0800

    Test nav backdrop

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -4,7 +4,7 @@ type Props = {};
 export default function Nav({}: Props) {
   return (
     <div className="fixed bottom-0 w-full">
-      <nav className="w-full max-w-md px-3 mx-auto bg-white/20 backdrop-blur-xl py-7">
+      <nav className="w-full max-w-md px-3 mx-auto bg-black/10 backdrop-blur-xl py-7">
         Nav
       </nav>
     </div>
