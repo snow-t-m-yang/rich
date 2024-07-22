3739b3a6d9e1dbe593168fc8bdd29ee7da9a3e73
Snow Yáng <snow.yang@fluv.com>
2024-07-22 20:54:38 +0800

    Execute p add jotai

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import Link from "next/link";
+import { LuPlus } from "react-icons/lu";
+
 type Props = {};
 export default function Nav({}: Props) {
   return (
     <div className="fixed bottom-0 w-full">
-      <nav className="w-full max-w-xl px-3 mx-auto bg-background/30 backdrop-blur-xl py-7">
-        Nav
+      <nav className="flex justify-center w-full max-w-xl px-3 mx-auto bg-background/30 backdrop-blur-xl py-7">
+        <button onMouseDown={(e) => console.log(e)}>
+          <LuPlus className="w-12 h-12" />
+        </button>
       </nav>
     </div>
   );
