1a45273067c523d59b6e5fde5b7e7fb25f9fde0d
Snow Yáng <snow.yang@fluv.com>
2024-07-22 21:20:41 +0800

    Add view atom

--- src/app/components/dynamic-banner.tsx
+++ src/app/components/dynamic-banner.tsx
@@ -1,15 +1,19 @@
-'use client';
+"use client";
 
+import { useAtomValue } from "jotai";
 import { usePathname } from "next/navigation";
+import { viewAtom } from "../atoms";
 
 type Props = {};
 export default function DynamicBanner({}: Props) {
   const pathname = usePathname();
+  const view = useAtomValue(viewAtom);
 
   return (
     <div className="fixed -top-5 w-full z-10">
       <div className="w-full max-w-xl rounded-t-3xl px-3 mx-auto bg-background/30 backdrop-blur-xl py-7">
         <h1>{pathname === "/" ? "Home" : pathname}</h1>
+        <p>{view}</p>
       </div>
     </div>
   );
--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { atom, useAtom } from "jotai";
 import Link from "next/link";
-import { LuPlus } from "react-icons/lu";
+import { LuPlus, LuX } from "react-icons/lu";
+import { viewAtom } from "../atoms";
 
 type Props = {};
 export default function Nav({}: Props) {
+  const [view, setView] = useAtom(viewAtom);
+  console.log(view);
   return (
     <div className="fixed bottom-0 w-full">
       <nav className="flex justify-center w-full max-w-xl px-3 mx-auto bg-background/30 backdrop-blur-xl py-7">
-        <button onMouseDown={(e) => console.log(e)}>
-          <LuPlus className="w-12 h-12" />
+        <button onMouseDown={() => setView(view === "add" ? "nil" : "add")}>
+          <LuPlus
+            className={`${
+              view === "add" ? "rotate-45" : ""
+            } w-12 h-12 duration-300 transition-all`}
+          />
         </button>
       </nav>
     </div>
