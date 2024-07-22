7fbf6d35bdf719d1082bf4589b9219fbeaf7582b
Snow Yáng <snow.yang@fluv.com>
2024-07-22 20:51:17 +0800

    Execute p add react-icons --save

--- src/app/components/dynamic-banner.tsx
+++ src/app/components/dynamic-banner.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import { usePathname } from "next/navigation";
+
 type Props = {};
 export default function DynamicBanner({}: Props) {
+  const pathname = usePathname();
+
   return (
     <div className="fixed -top-5 w-full z-10">
       <div className="w-full max-w-xl rounded-t-3xl px-3 mx-auto bg-background/30 backdrop-blur-xl py-7">
-        Banner
+        <h1>{pathname === "/" ? "Home" : pathname}</h1>
       </div>
     </div>
   );
